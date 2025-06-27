60fb64e83ff750ee9e00b4f08fe86916c260390a
jooaaao35 <joaof0666@gmail.com>
2025-06-27 20:25:48 -0300

    1

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let titulo = document.querySelector('h1');
 
 titulo.innerHTML = 'Jogo do numero secreto';
 
-let titulo = document.querySelector("p"); 
+let titulo = document.querySelector('p'); 
 
 titulo.innerHTML = 'Quero dormir';
