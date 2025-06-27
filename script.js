9bedc969e0473f26174cba8b48a413ec9bb13214
jooaaao35 <joaof0666@gmail.com>
2025-06-27 20:32:08 -0300

    10

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ titulo.innerHTML = 'Jogo do numero secreto';
 
 let titulo = document.querySelector('p'); 
 
-titulo.innerHTML = 'Quero dormir';
+paragrafo.innerHTML = 'Quero dormir';
+
+function iniciarJogo(){
+    let num1 = parseInt(prompt("Digite o primeiro numero inteiro:"));
+    let num2 = parseInt(prompt("Digite o segundo numero inteiro:"));
+
+
+    let resultado = num1 + num2;
+    alert("A soma dos numeros é: " + resultado);
+}
